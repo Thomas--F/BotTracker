d7ff0d8f6bb75e64ef7115c212c4371cb8409d2c
Thomas Fasselt <Thomas--F@users.noreply.github.com>
2023-01-16 19:10:35 +0100

    fix issue #99

--- Reports/GetTop10.php
+++ Reports/GetTop10.php
@@ -26,6 +26,7 @@ class GetTop10 extends Base
         parent::init();
 
         $this->name          = Piwik::translate('BotTracker_Top_10_Bots');
+	$this->subcategoryId = 'BotTracker';
         // This defines in which order your report appears in the mobile app, in the menu and in the list of widgets
         $this->order = 99;
 
